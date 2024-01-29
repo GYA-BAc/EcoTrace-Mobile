562c18c04334ccb45ca02d84288f2603a88d5ff0
JAlan <GeeWhyAlan@outlook.com>
2024-01-28 22:03:57 -0600

    added placeholder camera button

--- src/components/bottomMenu/bottomMenu.js
+++ src/components/bottomMenu/bottomMenu.js
@@ -1,42 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        // height: 100,
-        flexGrow: 1,
-        zIndex: 2,
-        bottom: 0,
-        backgroundColor: "#ddd",
+    parentContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      flexGrow: 1,
+      zIndex: 2,
+      bottom: 0,
+      backgroundColor: "#ddd",
+      alignItems: "center"
     },
+    cameraButton: {
+      width: 75,
+      height: 75,
+      backgroundColor: "#fff",
+      borderRadius: 50,
+      overflow: "hidden"
+    }
 });
 
 
 const BottomMenu = () => {
   return (
-    <View
-    style={styles.container}>
-    <TouchableOpacity
-      style={{ padding: 10 }}
-      onPress={() => {
-        alert('Left');
-      }}>
-      <Text>Left</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={{ padding: 10 }}
-      onPress={() => {
-        alert('Right');
-      }}>
-      <Text>Right</Text>
-    </TouchableOpacity>
+    <View style={styles.parentContainer}>
+      <TouchableOpacity
+        style={{ padding: 10 }}
+        onPress={() => {
+          alert('Left');
+        }}>
+        <Text>Left</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.cameraButton}
+        onPress={() => {
+          alert('Picture');
+        }}>
+        {/* <Text style={{backgroundColor: "#a00"}}>Camera</Text> */}
+        <ImageBackground 
+          resizeMode='stretch'
+          source={require('../../../assets/favicon.png')}
+          style={{flex: 1}}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{ padding: 10 }}
+        onPress={() => {
+          alert('Right');
+        }}>
+        <Text>Right</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
 
 
 export default BottomMenu;
