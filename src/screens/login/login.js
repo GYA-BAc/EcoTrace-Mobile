fa7d34dc681024a8b71abc0d3fdde2fe47956166
JAlan <GeeWhyAlan@outlook.com>
2024-03-25 18:26:11 -0500

    add login screen styling

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 
@@ -7,9 +7,44 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: "center"
+    },
+    title: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      letterSpacing: 2,
+      paddingBottom: 50
+    },
+    loginContainer: {
+      width: "70%",
+    },
+    credentials: {
+      alignItems: "center",
+    },
+    textInput: {
+      width: "100%",
+      height: 50,
+      borderColor: 'black',
+      borderWidth: 2,
+      borderRadius: 10,
+      marginBottom: 50,
+      paddingLeft: 5
+    },
+    actions: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    button: {
+      width: "45%",
+      borderColor: "#000",
+      borderWidth: 2,
+      borderRadius: 5,
+      height: 75,
       alignItems: 'center',
       justifyContent: 'center',
-    },
+    }
 });
 
 
@@ -28,9 +63,30 @@ const Login = () => {
     //   res => res.json()
     // )
 
+    fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/logout`).then(
+      (res) => {
+        if (!res.ok) {
+          console.log(res.status)
+          return
+        }
+        return res.json()
+      }
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+
 
     fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/fetchUserData`).then(
-      res => res.json()
+      (res) => {
+        if (!res.ok) {
+          console.log(res.status)
+          return
+        }
+        return res.json()
+      }
     ).then(
       data => {
         setData(data)
@@ -42,7 +98,28 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-    <Text>Login Here</Text>
+      <Text style={styles.title}>Login Here</Text>
+
+      <View style={styles.loginContainer}>
+
+        <SafeAreaView style={styles.credentials}>
+          <TextInput style={styles.textInput} placeholder="Username"/>
+          <TextInput secureTextEntry={true} style={styles.textInput} placeholder="Password"/>
+        </SafeAreaView>
+
+        <View style={styles.actions}>
+          <TouchableOpacity onpress={()=>{}} style={styles.button}> 
+            <Text>Sign Up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onpress={()=>{}} style={styles.button}> 
+            <Text>Log in</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+      
+
     </View>
   );
 }
