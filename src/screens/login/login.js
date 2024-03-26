da3ca624fddf19ca3023d047c559793bc3abf582
JAlan <GeeWhyAlan@outlook.com>
2024-03-25 19:04:09 -0500

    add redementary login capability

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -54,6 +54,12 @@ const Login = () => {
 
   const [data, setData] = useState([{}])
 
+  const usernameInput = useRef(null)
+  const [username, setUsername] = useState('')
+
+  const passwordInput = useRef(null)
+  const [password, setPassword] = useState('')
+
   useEffect( () => {
     // fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/login`, {
     //   method: "POST",
@@ -63,37 +69,72 @@ const Login = () => {
     //   res => res.json()
     // )
 
-    fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/logout`).then(
-      (res) => {
-        if (!res.ok) {
-          console.log(res.status)
-          return
-        }
-        return res.json()
-      }
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
+    // fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/logout`).then(
+    //   (res) => {
+    //     if (!res.ok) {
+    //       console.log(res.status)
+    //       return
+    //     }
+    //     return res.json()
+    //   }
+    // ).then(
+    //   data => {
+    //     setData(data)
+    //     console.log(data)
+    //   }
+    // )
 
 
-    fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/fetchUserData`).then(
-      (res) => {
-        if (!res.ok) {
-          console.log(res.status)
-          return
-        }
-        return res.json()
-      }
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
+    // fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/fetchUserData`).then(
+    //   (res) => {
+    //     if (!res.ok) {
+    //       console.log(res.status)
+    //       return
+    //     }
+    //     return res.json()
+    //   }
+    // ).then(
+    //   data => {
+    //     setData(data)
+    //     console.log(data)
+    //   }
+    // )
   }, [])
+
+  const incorrectLogin = () => {
+    passwordInput.current.clear()
+  }
+
+  const correctLogin = () => {
+    usernameInput.current.clear()
+    passwordInput.current.clear()
+    navigation.navigate("EcoTrace")
+  }
+
+  const attemptLogin = () => {
+    // console.log(username)
+    // usernameInput.current.clear()
+    fetch(`${process.env.EXPO_PUBLIC_API_URL}/auth/login`, {
+      method: "POST",
+      headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, 
+      body: JSON.stringify({'username': username, 'password': password})
+    }).then(
+      (res) => {
+        if (!res.ok) {
+          console.log(res.status)
+          incorrectLogin()
+          return
+        }
+        correctLogin()
+        return res.json()
+      }
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }
 
 
   return (
@@ -103,17 +144,38 @@ const Login = () => {
       <View style={styles.loginContainer}>
 
         <SafeAreaView style={styles.credentials}>
-          <TextInput style={styles.textInput} placeholder="Username"/>
-          <TextInput secureTextEntry={true} style={styles.textInput} placeholder="Password"/>
+
+          <TextInput 
+            placeholder="Username"
+            ref={usernameInput} 
+            autoCorrect={false}
+            autoCapitalize='none'
+            onChangeText={text => setUsername(text)}
+            style={styles.textInput} 
+          />
+
+          <TextInput 
+            placeholder="Password"
+            ref={passwordInput} 
+            autoCorrect={false}
+            autoCapitalize='none'
+            secureTextEntry={true} 
+            onChangeText={text => setPassword(text)}
+            style={styles.textInput} 
+          />
+        
         </SafeAreaView>
 
         <View style={styles.actions}>
-          <TouchableOpacity onpress={()=>{}} style={styles.button}> 
+
+          <TouchableOpacity onPress={()=>{}} style={styles.button}> 
             <Text>Sign Up</Text>
           </TouchableOpacity>
-          <TouchableOpacity onpress={()=>{}} style={styles.button}> 
+
+          <TouchableOpacity onPress={attemptLogin} style={styles.button}> 
             <Text>Log in</Text>
           </TouchableOpacity>
+
         </View>
 
       </View>
