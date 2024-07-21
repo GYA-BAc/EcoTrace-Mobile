e85805830e03c7b7738ad660e07f64fa7318cfb6
JAlan <GeeWhyAlan@outlook.com>
2024-07-21 11:01:58 -0500

    add basic post caching

--- src/screens/group/group.js
+++ src/screens/group/group.js
@@ -14,35 +14,113 @@ const styles = StyleSheet.create({
 });
 
 
-const SAVED = 3
-
 
 const GroupView = ({route}) => {
   const navigation = useNavigation();
 
   const groupID = route.params.groupID
-  // console.log(groupID)
 
   var [posts, setPosts] = useState([])
+  const [latestPostID, setLatestPostID] = useState(null)
+  const [oldestPostID, setOldestPostID] = useState(null)
 
-  // try loading from localstorage
-  // useEffect(() => {
-  //   AsyncStorage.getItem(`G${groupID}`).then(
-  //     (val) => {
-  //       setState(val)
-  //     }
-  //   ).catch(
-  //     () => {
-  //       console.log("Error retrieving from local storage")
-  //       setState(null)
-  //     }
-  //   )
+  const reloadPosts = async () => {
 
-  //   var upToDate = false
+    var localState = JSON.parse(await AsyncStorage.getItem(`Group${groupID}`))
 
-  //   if (val === null) return
-    
-  // }, [])
+    var latest = latestPostID
+
+    if (!(localState === null)) {
+        // check to make sure is up to date
+        
+        fetchWithTimeout(
+            `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchLatestPostID/${groupID}`
+        ).then(
+            (res) => {
+                if (!res.ok) {
+                    // TODO: add bad case where server fails
+                    // probably where fails to fetch due to incorrect session cookie
+                    console.log(res.status)
+                    throw Error("ServerError")
+                }
+
+                return res.json()
+            }
+        ).then(
+            (data) => {
+                latest = data.id
+                setLatestPostID(data.id)
+
+                if (localState.latestPostID === latest) {
+                    setPosts(localState.posts)
+                    setOldestPostID(localState.oldestPostID)
+                    return true
+                }
+
+                return false
+            }
+        ).then(
+            // where localstate either doesn't exist or is out of date
+            // overwrite localstorage
+            (localExists) => {
+                if (localExists) return
+
+                var start_id = null 
+
+                fetchWithTimeout(
+                  `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchLatestPostID/${groupID}`
+                ).then(
+                  (res) => {
+            
+                    if (!res.ok) {
+                      // TODO: add bad case where server fails
+                      // console.log(res.status)
+                      throw "ServerError"
+                    }
+                    return res.json()
+                  }
+                ).then(
+                  (data) => {
+                    // console.log(data)
+                    start_id = data.id
+            
+                    if (start_id === null) {
+                      throw "could not fetch latest post id"
+                    }
+                    return start_id
+                  }
+                ).then(
+                  (start_id) => {
+                    fetchWithTimeout(
+                      `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchPostRange/${groupID}?`
+                      + new URLSearchParams({start_id: start_id, requested_posts: 15})
+                    ).then(
+                      (res) => {
+                        if (!res.ok) {
+                          // TODO: add bad case where server fails
+                          throw "ServerError"
+                        }
+                        return res.json()
+                      }
+                    ).then(
+                      async (data) => {
+                        setPosts(await asyncFetchPosts(data.map((item) => {return item.id})))
+                      }
+                    )
+                  }
+                ).catch(
+                    (e) => {
+                        console.log(e)
+                    }
+                )
+            }
+        ).catch(
+            (e) => {
+                console.log(e)
+            }
+        )
+      }
+  }
 
   useEffect(() => {
 
@@ -74,55 +152,24 @@ const GroupView = ({route}) => {
   // console.log(route)
 
   useEffect(() => {
-
-    var start_id = null 
-    
-    fetchWithTimeout(
-      `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchLatestPostID/${groupID}`
-    ).then(
-      (res) => {
-
-        if (!res.ok) {
-          // TODO: add bad case where server fails
-          // console.log(res.status)
-          throw "ServerError"
-        }
-        return res.json()
-      }
-    ).then(
-      (data) => {
-        // console.log(data)
-        start_id = data.id
-
-        if (start_id === null) {
-          throw "could not fetch latest post id"
-        }
-        return start_id
-      }
-    ).then(
-      (start_id) => {
-        fetchWithTimeout(
-          `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchPostRange/${groupID}?`
-          + new URLSearchParams({start_id: start_id, requested_posts: 15})
-        ).then(
-          (res) => {
-    
-            if (!res.ok) {
-              // TODO: add bad case where server fails
-              throw "ServerError"
-            }
-            return res.json()
-          }
-        ).then(
-          async (data) => {
-            setPosts(await asyncFetchPosts(data.map((item) => {return item.id})))
-            console.log(posts)
-          }
-        )
-      }
-    )
-    
+    (async () => {
+      await reloadPosts()
+    })()
   }, [])
+
+
+  useEffect(() => {
+    (async () => {
+      await AsyncStorage.setItem(
+        `Group${groupID}`, 
+        JSON.stringify({
+            posts: posts,
+            latestPostID: latestPostID,
+            oldestPostID: oldestPostID
+        })
+      )
+    })()
+  }, [posts, latestPostID, oldestPostID])
 
   return (
     <View style={styles.baseContainer}>
